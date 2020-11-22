38be771b327fedf2032345a397730bf6510e4c3d
William Valter Ambrósio Feijó <50035088@iade.pt>
2020-11-22 14:04:35 +0000

    Atualização do código anterior
    Modifiquei o codigo no StudentController e criei a classe NotFoundExceptions.

--- src/main/java/pt/iade/unimage/controllers/StudentController.java
+++ src/main/java/pt/iade/unimage/controllers/StudentController.java
@@ -2,28 +2,29 @@ package pt.iade.unimage.controllers;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
 import pt.iade.unimage.models.Student;
 import pt.iade.unimage.models.StudentRepository;
+import pt.iade.unimage.models.exceptions.NotFoundException;
 
 @RestController
 @RequestMapping(path="/api/students")
 public class StudentController {
     private Logger logger = LoggerFactory.getLogger(StudentController.class);
     
-    @RequestMapping(path="", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(path="", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Student> getStudents(){
         logger.info("Sending all students");
         return StudentRepository.getStudents();
     }
 
-    @RequestMapping(path="{number}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(path="{number}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Student getStudent(@PathVariable("number") int number) throws NotFoundException{
         logger.info("Sending the student with the number "+number);
         Student student = StudentRepository.getStudent(number);
@@ -31,25 +32,17 @@ public class StudentController {
         else throw new NotFoundException(""+number, "Student", "number");
     }
 
-    @RequestMapping(path="{number}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public boolean deleteStudent(@PathVariable("number") int number){
+    @DeleteMapping(path="{number}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response deleteStudent(@PathVariable("number") int number) {
         logger.info("Deleting the student with the number "+number);
         if(StudentRepository.deleteStudent(number))
             return new Response(number+" was deleted.",null);
         else return new Response(number+" not found.",null);
     }
 
-    @RequestMapping(path="", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(path="", produces = MediaType.APPLICATION_JSON_VALUE)
     public Student addStudent(@PathVariable("student") Student student){
         logger.info("Adding the student "+student);
         return StudentRepository.addStudent(student);
-    }
-
-    @ResponseStatus(value= HttpStatus.NOT_FOUND)
-    public class NotFoundException extends RuntimeException{
-        private static final long serialVersionUID = 5405519104069955535L;
-        public NotFoundException(String id, String elemType, String idName){
-            super(elemType+" with "+idName+" "+id+" not found.");
-        }
     }
 }
